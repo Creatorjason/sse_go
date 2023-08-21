ef215a53bbed186dd664582d9ef48294655b7544
Creatorjason <107406861+Creatorjason@users.noreply.github.com>
2023-08-21 23:35:58 +0100

    feat: created controller for /subscribe for clients to get message from server

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -14,8 +14,40 @@ var (
 
 
 func HandleReceiveMessage(c *gin.Context){
+	// Set up SSE
+	c.Header("Content-type", "text/event-stream")
+	c.Header("Cache-Control", "no-cache")
+	c.Header("Connection", "keep-alive")
 
-	
+
+	for _, message := range messages{
+		c.Writer.Write([]byte(fmt.Sprintf("ID: %d\n", message.ID)))
+		c.Writer.Write([]byte(fmt.Sprintf("Sender: %s\n", message.Sender)))
+		c.Writer.Write([]byte(fmt.Sprintf("Text: %s\n", message.Text)))
+		c.Writer.Write([]byte(fmt.Sprintf("CreatedAt: %s\n", message.CreatedAt)))
+
+		c.Writer.Write([]byte("-----------------------\n"))
+	}
+	c.Writer.Flush()
+
+	// create new channel for user
+	messageChan := make(chan models.Message)
+	clients = append(clients, messageChan)
+	for{
+		select{
+		case sseMsg :=  <- messageChan:
+			c.Writer.Write([]byte(fmt.Sprintf("ID: %d\n", sseMsg.ID)))
+            c.Writer.Write([]byte(fmt.Sprintf("Sender: %s\n", sseMsg.Sender)))
+            c.Writer.Write([]byte(fmt.Sprintf("Message: %s\n", sseMsg.Text)))
+            c.Writer.Write([]byte(fmt.Sprintf("Created Time: %s\n", sseMsg.CreateTime)))
+            c.Writer.Write([]byte("-----------------------\n"))
+            c.Writer.Flush()
+		case <-c.Writer.CloseNotify():
+			close(messageChan)
+			clients = removeClient(clients, messageChan)
+			return
+		}
+	}
 }
 
 func HandleSendMessage(c *gin.Context){
@@ -37,4 +69,13 @@ func HandleSendMessage(c *gin.Context){
 
 	c.JSON(http.StatusOk, msg)
 
+}
+func removeClient(clients []chan models.Message, client chan models.Message) []chan models.Message {
+    for i, c := range clients {
+        if c == client {
+            clients[i] = clients[len(clients)-1]
+            return clients[:len(clients)-1]
+        }
+    }
+    return clients
 }
